d484f5d44cf5d842f2df5b10447a5e92b3f56368
daniil <dt.bedrin@interfax.ru>
2026-02-24 21:39:11 +0300

    - fix homework(again_2)

--- niffler-e-2-e-tests/tests/test_api_spendings.py
+++ niffler-e-2-e-tests/tests/test_api_spendings.py
@@ -7,6 +7,6 @@ from test_data import TestSpendings
 @TestData.spends(TestSpendings.TestDataSpend)
 def test_spending_should_be_deleted(page, category, spends, main_page):
     page.reload()
-    main_page.expect_content_of_table("QA>GURU Python Advanced 6")
+    main_page.expect_content_of_table(TestSpendings.description)
     main_page.delete_spend(TestSpendings.TEST_CATEGORY_1)
-    main_page.expect_content_of_table_is_empty("QA>GURU Python Advanced 6")
+    main_page.expect_content_of_table_is_empty(TestSpendings.description)
