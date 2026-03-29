177b9392a5dd6503419b8fe6be92e999b3f1cda9
daniil <dt.bedrin@interfax.ru>
2026-03-30 02:00:39 +0300

    -add fix for yesterday_day paste date in utc

--- niffler-e-2-e-tests/utils/datatime_util.py
+++ niffler-e-2-e-tests/utils/datatime_util.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 
 def get_past_date_iso(days_ago: int = 7) -> str:
@@ -8,5 +8,7 @@ def get_past_date_iso(days_ago: int = 7) -> str:
 
 def get_past_date_str(days_ago: int = 1) -> str:
     """Возвращает день месяца в виде строки (для выбора в календаре UI)."""
-    past_date = datetime.now() - timedelta(days=days_ago)
-    return str(past_date.day)
+    utc_now = datetime.now(timezone.utc)
+    yesterday = utc_now - timedelta(days=1)
+    return str(yesterday.day)
+
